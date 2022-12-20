8199508805d80ffeeb242efc40b4e9c300e540ac
Issac <>
2022-12-20 17:46:14 +0800

    Init

---
+++ _assets/index-legacy.c9361ea5.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy.70366e3d.js"],(function(e,t){"use strict";var r;return{setters:[e=>{r=e.e}],execute:function(){e("g",(e=>{r.emit("router-goto",e)}))}}}));
---
+++ _assets/index.1b577d81.js
@@ -0,0 +1,1 @@
+import{e as t}from"./index.754fcb64.js";const r=o=>{t.emit("router-goto",o)};export{r as g};
