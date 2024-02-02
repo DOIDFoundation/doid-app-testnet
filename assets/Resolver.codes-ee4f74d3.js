e95d83e4d65f74acd11a13d44a6fedfd81e36a18
mac <mac@gmail.com>
2024-02-02 16:44:06 +0800

    update

--- assets/Resolver.codes-ee4f74d3.js
+++
@@ -1,1 +0,0 @@
-const e="IC",o={IC:e};export{e as IC,o as default};
--- assets/Resolver.codes-legacy.77c566ca.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(t,e){"use strict";return{execute:function(){const e=t("IC","IC");t("default",{IC:e})}}}));
